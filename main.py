9cb7c65f42706d2e396089755c1273ff0f33e291
Jacky2 <jackyyeh511@gmail.com>
2023-12-14 16:46:52 -0600

    Integrate code
    1. Add feature detector & matching
    2. Add nister's 5 points & 8 points

--- main.py
+++ main.py
@@ -1,117 +1,107 @@
-import numpy as np
-import cv2
-from monovideoodometery import MonoVideoOdometery
 import os
+import cv2
 import argparse
+import numpy as np
+from time import time
+from mono_visual_odometry import MonoVisualOdometery
 import pathlib
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--feature', '-f', type=str, choices=['sift', 'fast', 'orb'])
-parser.add_argument('--target_num', '-n', type=int, default=-1, help='-1 means that all frames should be processed')
-parser.add_argument('--vis', action='store_true', help='visualize intermediate result')
-parser.add_argument('--optimize', action='store_true', help='enable pose graph optimization')
-parser.add_argument('--local_window', default=5, type=int, help='number of frames to run the optimization')
-parser.add_argument('--num_iter', default=100, type=int, help='number of max iterations to run the optimization')
+parser.add_argument('--detector', '-d', type=str,
+                    default='FAST', choices=['FAST', 'BRISK', 'ORB', 'SIFT'])
+parser.add_argument('--matcher', '-m', type=str,
+                    default='LK', choices=['LK', 'BF', 'FLANN'])
+parser.add_argument('--target_num', '-n', type=int, default=-1,
+                    help='-1 means that all frames should be processed')
+parser.add_argument('--ransac', '-r', type=int, default=5, choices=[5, 8])
 
-OUTPUT_DIR = 'output'
+OUTPUT_DIR = 'results'
 
 def main():
     args = parser.parse_args()
-
-    print('======= Initial arguments =======')
-    for key, val in vars(args).items():
-        print("name: {} => {}".format(key, val))
-
-    img_path = './dataset/sequences/00/image_0/'
-    pose_path = './dataset/poses/00.txt'
-    
-    focal = 718.8560
-    pp = (607.1928, 185.2157)
-    R_total = np.zeros((3, 3))
-    t_total = np.empty(shape=(3, 1))
-
-    # Parameters for lucas kanade optical flow
-    lk_params = dict(winSize=(21, 21),
-                    criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
-
-
-    # Create some random colors
-    color = np.random.randint(0, 255, (5000, 3))
-
-    detector = cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
-    vo = MonoVideoOdometery(args, img_path, pose_path, detector, focal, pp, lk_params)
-    traj = np.zeros(shape=(600, 800, 3))
-
-    # mask = np.zeros_like(vo.current_frame)
-    # flag = False
-    cnt = 0
-    rmse_errors = []
-    while (vo.hasNextFrame()):
-
-        cnt += 1
-        if cnt == args.target_num + 1:
-            break
-
-        print("\n----- Processing frames {} -----".format(cnt))
-        frame = vo.current_frame
-
-        vo.process_frame()
-
-        print(vo.get_mono_coordinates())
-
-        mono_coord = vo.get_mono_coordinates()
-        true_coord = vo.get_true_coordinates()
-
-        rmse_error = np.linalg.norm(mono_coord - true_coord)
-        rmse_errors.append(rmse_error)
-        print("MSE Error: ", rmse_error)
-        print("x: {}, y: {}, z: {}".format(*[str(pt) for pt in mono_coord]))
-        print("true_x: {}, true_y: {}, true_z: {}".format(
-            *[str(pt) for pt in true_coord]))
-
-        draw_x, draw_y, draw_z = [int(round(x)) for x in mono_coord]
-        true_x, true_y, true_z = [int(round(x)) for x in true_coord]
-
-        traj = cv2.circle(traj, (true_x + 400, true_z + 100),
-                        1, list((0, 0, 255)), 4)
-        traj = cv2.circle(traj, (draw_x + 400, draw_z + 100),
-                        1, list((0, 255, 0)), 4)
-
-        cv2.putText(traj, 'Actual Position:', (140, 90),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-        cv2.putText(traj, 'Red', (270, 90),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
-        cv2.putText(traj, 'Estimated Odometry Position:', (30, 120),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-        cv2.putText(traj, 'Green', (270, 120),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-
-        if args.vis:
-            cv2.imshow('frame', frame)
-            k = cv2.waitKey(1)
-            if k == 27:
-                break
-
-            cv2.imshow('trajectory', traj)
-
-        if cnt % 100 == 0:
-            output_path = os.path.join(
-                OUTPUT_DIR, 'trajectory_tmp_{}.png'.format(cnt))
-            cv2.imwrite(output_path, traj)
-
-    mean_rmse_error = np.mean(rmse_errors)
-    print('avg rmse_error:', mean_rmse_error)
     pathlib.Path(OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
     
-    # display avg rmse error
-    # Put the text on the image
-    cv2.putText(traj, 'avg rmse_error: {:.3f}'.format(mean_rmse_error), (140, 50),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-    
-    output_path = os.path.join(OUTPUT_DIR, 'trajectory_w{}_i{}.png'.format(
-        args.local_window, args.num_iter))
-    cv2.imwrite(output_path, traj)
-    cv2.destroyAllWindows()
+    log = open('./{}/{}+{}.txt'.format(OUTPUT_DIR, args.detector, args.matcher), 'w')
+    img_path = './dataset/sequences/00/image_0/'
+    pose_path = './dataset/poses/00.txt'
+    focal = 718.8560
+    pp = (607.1928, 185.2157)
+    vo = MonoVisualOdometery(
+        img_path, pose_path, args.detector, args.matcher, focal, pp, args.ransac)
+    traj = np.zeros(shape=(600, 800, 3))
+
+    f_num = 0
+    f_tot = len(os.listdir(img_path)) if args.target_num == - \
+        1 else args.target_num
+    errors = []
+    flag = False
+    try:
+        start_time = time()
+        while (vo.hasNextFrame()):
+            frame = vo.current_frame
+            cv2.imshow('frame', frame)
+            k = cv2.waitKey(1)
+
+            if k == 27:
+                break
+            if k == 121:
+                flag = not flag
+                def toggle_out(flag): return "On" if flag else "Off"
+                print("Flow lines turned ", toggle_out(flag))
+
+            vo.process_frame()
+            mono_coord = vo.get_mono_coordinates()
+            true_coord = vo.get_true_coordinates()
+            dist = np.linalg.norm(mono_coord - true_coord)
+            errors.append(dist)
+
+            print('========  Processing frame {}  ========'.format(f_num))
+            print('pred_x: {}, pred_y: {}, pred_z: {}'.format(
+                *[str(round(pt, 2)) for pt in mono_coord]))
+            print('true_x: {}, true_y: {}, true_z: {}'.format(
+                *[str(round(pt, 2)) for pt in true_coord]))
+            print('Pose Error: {}\n'.format(dist))
+
+            log.write(str(f_num)+' '+str(mono_coord[0])+' '+str(mono_coord[1])+' '+str(mono_coord[2])
+                      + ' '+str(true_coord[0])+' '+str(true_coord[1])+' '+str(true_coord[2])+'\n')
+
+            draw_x, draw_y, draw_z = [int(round(x)) for x in mono_coord]
+            true_x, true_y, true_z = [int(round(x)) for x in true_coord]
+            traj = cv2.circle(traj, (true_x+400, true_z+100),
+                              1, list((0, 0, 255)), 4)
+            traj = cv2.circle(traj, (draw_x+400, draw_z+100),
+                              1, list((0, 255, 0)), 4)
+
+            cv2.putText(traj, 'Ground True Trajectory:', (60, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+            cv2.putText(traj, 'Red', (260, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            cv2.putText(traj, 'Estimated Trajectory:', (60, 90),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+            cv2.putText(traj, 'Green', (240, 90),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+            cv2.imshow('trajectory', traj)
+
+            f_num = f_num + 1
+            if f_num >= f_tot:
+                break
+
+        log.close()
+        end_time = time()
+        cv2.putText(traj, 'Average RMSE Error: {:.3f} m'.format(np.mean(errors)), (60, 120),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+        cv2.putText(traj, 'Elapsed Time: {:.3f} s'.format(end_time - start_time), (60, 150),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+        cv2.imwrite(
+            "./{}/{}+{}.png".format(OUTPUT_DIR, args.detector, args.matcher), traj)
+        cv2.destroyAllWindows()
+
+    except KeyboardInterrupt:
+        log.close()
+        cv2.imwrite(
+            "./{}/{}+{}.png".format(OUTPUT_DIR, args.detector, args.matcher), traj)
+        cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
---
+++ mono_visual_odometry.py
@@ -0,0 +1,275 @@
+import os
+import cv2
+import numpy as np
+import cv2
+import os
+from optimizer import PoseGraphOptimization
+from utils import *
+
+
+
+class MonoVisualOdometery(object):
+    def __init__(self, 
+                 img_file_path, 
+                 pose_file_path, 
+                 detector, 
+                 matcher, 
+                 focal_length, 
+                 pp, 
+                 ransac=5):
+        '''
+        Arguments:
+            img_file_path {str} -- File path that leads to image sequences
+            pose_file_path {str} -- File path that leads to true poses from image sequence
+        Keyword Arguments:
+            focal_length {float} -- Focal length of camera used in image sequence (default: {718.8560})
+            pp {tuple} -- Principal point of camera in image sequence (default: {(607.1928, 185.2157)})
+            lk_params {dict} -- Parameters for Lucas Kanade optical flow (default: {dict(winSize  = (21,21), criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))})
+            detector {cv2.FeatureDetector} -- Most types of OpenCV feature detectors (default: {cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)})
+        Raises:
+            ValueError -- Raised when file either file paths are not correct, or img_file_path is not configured correctly
+        '''
+        if detector == 'FAST' and matcher != 'LK':
+            raise ValueError(
+                'FAST is not a keypoint descriptor and can only be used with Lucas–Kanade.')
+
+        if detector == 'FAST':
+            self.detector = cv2.FastFeatureDetector_create(
+                threshold=25, nonmaxSuppression=True)
+        elif detector == 'BRISK':
+            self.detector = cv2.BRISK_create(thresh=25)
+        elif detector == 'ORB':
+            self.detector = cv2.ORB_create(nfeatures=1000, fastThreshold=20)
+        elif detector == 'SIFT':
+            self.detector = cv2.SIFT_create(nfeatures=2000)
+        else:
+            raise ValueError('Unknown detector type: {}'.format(detector))
+
+        if matcher == 'LK':
+            self.lk_params = dict(winSize=(21, 21), criteria=(
+                cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
+        elif matcher == 'BF':
+            if detector == 'SIFT':
+                self.matcher = cv2.BFMatcher()
+            else:
+                self.matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        elif matcher == 'FLANN':
+            if detector == 'SIFT':
+                index_params = dict(algorithm=1, trees=5)
+            else:
+                index_params = dict(algorithm=6, table_number=6,
+                                    key_size=12, multi_probe_level=2)
+            search_params = dict(checks=50)
+            self.matcher = cv2.FlannBasedMatcher(index_params, search_params)
+        else:
+            raise ValueError('Unknown matcher type: {}'.format(matcher))
+
+        self.ransac = ransac
+        self.RANSAC_NUM_ITERATIONS = 25
+        self.RANSAC_THRESHOLD = 1
+        self.detector_name = detector
+        self.matcher_name = matcher
+        self.kptdes = {}
+        self.file_path = img_file_path
+        self.focal = focal_length
+        self.pp = pp
+        self.R = np.zeros(shape=(3, 3))
+        self.t = np.zeros(shape=(3, 3))
+        self.id = 0
+        self.n_features = 0
+        self.img_shape = cv2.imread(
+            self.file_path + str().zfill(6) + '.png', 0).shape
+        self.K = np.array([[7.215377000000e02, 0.000000000000e00, 6.095593000000e02],
+                           [0.000000000000e00, 7.215377000000e02, 1.728540000000e02],
+                           [0.000000000000e00, 0.000000000000e00, 1.000000000000e00]])
+        try:
+            if not all([".png" in x for x in os.listdir(img_file_path)]):
+                raise ValueError(
+                    "img_file_path is not correct and does not exclusively png files")
+        except Exception as e:
+            print(e)
+            raise ValueError(
+                "The designated img_file_path does not exist, please check the path and try again")
+        try:
+            with open(pose_file_path) as f:
+                self.pose = f.readlines()
+        except Exception as e:
+            print(e)
+            raise ValueError(
+                "The pose_file_path is not valid or did not lead to a txt file")
+        self.process_frame()
+
+    def detect(self, img):
+        '''Used to detect features and parse into useable format
+        Arguments:
+            img {np.ndarray} -- Image for which to detect keypoints on
+        Returns:
+            np.array -- A sequence of points in (x, y) coordinate format
+            denoting location of detected keypoint
+        '''
+        keypoints, descriptors = self.detector.detectAndCompute(img, None)
+        keypoints = np.array([x.pt for x in keypoints], dtype=np.float64)
+        return {'keypoints': keypoints, 'descriptors': descriptors}
+
+    def match(self, kptdes):
+        good = []
+        if self.matcher_name == 'BF':
+            matches = self.matcher.match(
+                kptdes['ref']['descriptors'], kptdes['cur']['descriptors'])
+            # in the order of their distance.
+            matches = sorted(matches, key=lambda x: x.distance)
+            for i in range(300):
+                good.append([matches[i]])
+
+        elif self.matcher_name == 'FLANN' or self.detector_name == 'SIFT':
+            matches = self.matcher.knnMatch(
+                kptdes['ref']['descriptors'], kptdes['cur']['descriptors'], k=2)
+            for m, n in matches:  # Apply ratio test
+                if m.distance < 0.75 * n.distance:
+                    good.append([m])
+            # in the order of their distance.
+            good = sorted(good, key=lambda x: x[0].distance)
+
+        kp_ref = np.zeros([len(good), 2])
+        kp_cur = np.zeros([len(good), 2])
+        match_dist = np.zeros([len(good)])
+        for i, m in enumerate(good):
+            kp_ref[i, :] = kptdes['ref']['keypoints'][m[0].queryIdx]
+            kp_cur[i, :] = kptdes['cur']['keypoints'][m[0].trainIdx]
+            match_dist[i] = m[0].distance
+        return kp_ref, kp_cur
+
+    def visual_odometery(self):
+        '''
+        Used to perform visual odometery. If features fall out of frame
+        such that there are less than 2000 features remaining, a new feature
+        detection is triggered. 
+        '''
+        if self.detector_name == 'FAST' and self.matcher_name == 'LK':
+            if self.n_features < 2000:
+                kps = self.detector.detect(self.old_frame)
+                self.p0 = np.array([x.pt for x in kps],
+                                   dtype=np.float32).reshape(-1, 1, 2)
+
+            # Calculate optical flow between frames, st holds status of points from frame to frame
+            self.p1, st, err = cv2.calcOpticalFlowPyrLK(
+                self.old_frame, self.current_frame, self.p0, None, **self.lk_params)
+
+            # Save the good points from the optical flow
+            self.good_old = self.p0[st == 1]
+            self.good_new = self.p1[st == 1]
+
+            # Save the total number of good features
+            self.n_features = self.good_new.shape[0]
+
+        else:
+            # update keypoints and descriptors
+            if self.id == 0:
+                self.kptdes['ref'] = self.detect(self.old_frame)
+            else:
+                self.kptdes['ref'] = self.kptdes['cur']
+            self.kptdes['cur'] = self.detect(self.current_frame)
+
+            # match keypoints
+            self.good_old, self.good_new = self.match(self.kptdes)
+
+        # compute relative R, t between ref and cur frame
+        if self.ransac == 5:
+            E, _ = cv2.findEssentialMat(
+                self.good_new, self.good_old, self.focal, self.pp, cv2.RANSAC, 0.999, 1.0, None)
+        elif self.ransac == 8:
+            E = self.findEssentialMat()
+        _, R, t, _ = cv2.recoverPose(
+            E, self.good_old, self.good_new, focal=self.focal, pp=self.pp, mask=None)
+
+        # If the frame is one of first two, we need to initalize our t and R vectors
+        if self.id < 2:
+            self.R = R
+            self.t = t
+        else:
+            # get absolute pose based on absolute_scale
+            absolute_scale = self.get_absolute_scale()
+            if (absolute_scale > 0.1 and abs(t[2][0]) > abs(t[0][0]) and abs(t[2][0]) > abs(t[1][0])):
+                self.t = self.t + absolute_scale * self.R @ t
+                self.R = R @ self.R
+
+    def get_mono_coordinates(self):
+        # We multiply by the diagonal matrix to fix our vector onto same coordinate axis as true values
+        diag = np.array([[-1, 0, 0], [0, -1, 0], [0, 0, -1]])
+        adj_coord = np.matmul(diag, self.t)
+        return adj_coord.flatten()
+
+    def get_true_coordinates(self):
+        '''Returns true coordinates of vehicle
+        Returns:
+            np.array -- Array in format [x, y, z]
+        '''
+        return self.true_coord.flatten()
+
+    def get_absolute_scale(self):
+        '''Used to provide scale estimation for mutliplying
+           translation vectors
+        Returns:
+            float -- Scalar value allowing for scale estimation
+        '''
+        pose = self.pose[self.id - 1].strip().split()
+        x_prev = float(pose[3])
+        y_prev = float(pose[7])
+        z_prev = float(pose[11])
+        pose = self.pose[self.id].strip().split()
+        x = float(pose[3])
+        y = float(pose[7])
+        z = float(pose[11])
+
+        true_vect = np.array([[x], [y], [z]])
+        self.true_coord = true_vect
+        prev_vect = np.array([[x_prev], [y_prev], [z_prev]])
+        return np.linalg.norm(true_vect - prev_vect)
+
+    def hasNextFrame(self):
+        '''Used to determine whether there are remaining frames
+           in the folder to process
+        Returns:
+            bool -- Boolean value denoting whether there are still 
+            frames in the folder to process
+        '''
+        return self.id < len(os.listdir(self.file_path))
+
+    def process_frame(self):
+        '''Processes images in sequence frame by frame
+        '''
+        if self.id < 2:
+            self.old_frame = cv2.imread(
+                self.file_path + str().zfill(6)+'.png', 0)
+            self.current_frame = cv2.imread(
+                self.file_path + str(1).zfill(6)+'.png', 0)
+            self.visual_odometery()
+            self.id = 2
+        else:
+            self.old_frame = self.current_frame
+            self.current_frame = cv2.imread(
+                self.file_path + str(self.id).zfill(6)+'.png', 0)
+            self.visual_odometery()
+            self.id += 1
+
+    def findEssentialMat(self):
+
+        fundamental_matrix, self.good_old, self.good_new = self.RANSAC_8pt(
+            self.good_old, self.good_new)
+        E = self.K.T @ fundamental_matrix @ self.K
+        return E
+
+    def RANSAC_8pt(self, previous_frame_points, current_frame_points):
+        """
+        Open CV Eight point estimation of fundamental matrix
+        :param previous_frame_points_points: Previous frame matched key points
+        :param current_frame_points: Current frame matched key points
+        :return: Fundamental matrix, inlier points
+        """
+        _, inliers = cv2.findFundamentalMat(
+            previous_frame_points, current_frame_points, cv2.FM_RANSAC)
+        previous_frame_inliers = previous_frame_points[inliers.ravel() == 1]
+        current_frame_inliers = current_frame_points[inliers.ravel() == 1]
+        F, _ = cv2.findFundamentalMat(
+            previous_frame_inliers, current_frame_inliers, cv2.FM_8POINT)
+        return F.T, previous_frame_inliers, current_frame_inliers
